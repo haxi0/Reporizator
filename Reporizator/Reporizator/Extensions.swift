a855677103d7d510cb532cfd6393930e32c3457d
haxi0 <85764897+haxi0@users.noreply.github.com>
2022-11-06 18:47:51 +0300

    1.0

---
+++ Reporizator/Reporizator/Extensions.swift
@@ -0,0 +1,31 @@
+//
+//  Extensions.swift
+//  Reporizator
+//
+//  Created by Анохин Юрий on 06.11.2022.
+//
+
+import SwiftUI
+
+public struct CustomButtonStyle: ButtonStyle {
+    public func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .font(Font.body.weight(.medium))
+            .padding(.vertical, 12)
+            .foregroundColor(Color.white)
+            .frame(maxWidth: .infinity)
+            .background(
+                RoundedRectangle(cornerRadius: 14.0, style: .continuous)
+                    .fill(Color.orange)
+            )
+            .opacity(configuration.isPressed ? 0.4 : 1.0)
+    }
+}
+
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
---
+++ Reporizator/Reporizator/ReporizatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReporizatorApp.swift
+//  Reporizator
+//
+//  Created by Анохин Юрий on 06.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct ReporizatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
